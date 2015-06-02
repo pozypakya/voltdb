282f7b554ed2f8b217c1c980427a3b07e250b51b
jcrawford <jcrawford@voltdb.com>
2015-06-02 11:04:07 -0400

    index-count1.sql: added a few query patterns that will produce NPE's & mismatches; these are only temporary, for testing that the results look correct on Jenkins.

--- tests/scripts/examples/sql_coverage/index-count1.sql
+++ tests/scripts/examples/sql_coverage/index-count1.sql
@@ -13,3 +13,18 @@ SELECT COUNT(*) FROM _table WHERE POINTS BETWEEN 1 and 7
 SELECT COUNT(*) FROM _table WHERE POINTS BETWEEN 1 and 9
 SELECT COUNT(*) FROM _table WHERE POINTS BETWEEN 3 and 7
 SELECT COUNT(*) FROM _table WHERE POINTS BETWEEN 3 and 9
+
+-- TODO: temp, for sqlCoverage testing:
+SELECT * FROM _table
+
+-- Causes NPE in HsqlDb (ENG-8292):
+SELECT ID, (SELECT SUM(ID) FROM _table WHERE A2.ID = ID) FROM _table AS A2 GROUP BY ID HAVING SUM(ID) = 12
+
+-- May cause NPE in HsqlDb (ENG-8292):
+--SELECT ID, (SELECT SUM(ID) FROM _table WHERE A2.ID = ID) FROM _table AS A2 GROUP BY ID HAVING SUM(ID) = 12
+
+-- May cause NPE in HsqlDb (ENG-8381):
+--SELECT PKEY FROM A UNION ALL SELECT I FROM B UNION ALL SELECT I FROM C ORDER BY PKEY OFFSET 3
+
+-- May cause NPE in HsqlDb (ENG-8273):
+--select A.ID, count(*), (SELECT max(ID) FROM R2 B where B.count = A.count) FROM _table A GROUP BY ID, count order by ID, count
