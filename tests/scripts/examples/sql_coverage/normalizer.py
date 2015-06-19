98518074b55e5fb4975d4551687253883053f26e
jcrawford <jcrawford@voltdb.com>
2015-06-18 23:08:41 -0400

    normalizer.py: removed debug print statements.

--- tests/scripts/examples/sql_coverage/normalizer.py
+++ tests/scripts/examples/sql_coverage/normalizer.py
@@ -124,10 +124,6 @@ def move_rows(rows, start, end, moveto):
        (inclusive) and ending with index 'end' (exclusive) to a position
        just before index 'moveto', and returns the result.
     """
-    # TODO: temp debug!
-    print '      In move_rows...'
-    print '        start, end, moveto, len(rows):', start, end, moveto, len(rows)
-    print '        rows0:', rows
     if start < 0 or end < 0 or start > len(rows) or end > len(rows) or start > end:
         raise ValueError('Illegal value of start (%d) or end (%d): negative, greater than len(rows) (%d), or start > end' % (start, end, len(rows)))
     elif moveto < 0 or (moveto > start and moveto < end) or moveto > len(rows):
@@ -136,38 +132,14 @@ def move_rows(rows, start, end, moveto):
         return
 
     newrows = []
-    # TODO: temp debug!
-    #print '        newrows0 :', newrows
-    #print '        rows[0:min(moveto,start)]   :', rows[0:min(moveto,start)]
     newrows.extend(rows[0:min(moveto,start)])
-    # TODO: temp debug!
-    #print '        newrows1 :', newrows
     if moveto < start:
-        # TODO: temp debug!
-        #print '        rows[start:end]   :', rows[start:end]
         newrows.extend(rows[start:end])
-        # TODO: temp debug!
-        #print '        newrows2a:', newrows
-        #print '        rows[moveto:start]:', rows[moveto:start]
         newrows.extend(rows[moveto:start])
-        # TODO: temp debug!
-        #print '        newrows3a:', newrows
     elif moveto > end:
-        # TODO: temp debug!
-        #print '        rows[end:moveto]  :', rows[end:moveto]
         newrows.extend(rows[end:moveto])
-        # TODO: temp debug!
-        #print '        newrows2b:', newrows
-        #print '        rows[start:end]   :', rows[start:end]
         newrows.extend(rows[start:end])
-        # TODO: temp debug!
-        #print '        newrows3b:', newrows
-    # TODO: temp debug!
-    #print '        rows[max(moveto,end):len(rows)]:', rows[max(moveto,end):len(rows)]
     newrows.extend(rows[max(moveto,end):len(rows)])
-    # TODO: temp debug!
-    print '        newrows4 :', newrows
-    #return newrows
     rows[0:len(newrows)] = newrows[0:len(newrows)]
 
 class SortNulls:
@@ -186,12 +158,6 @@ def sort(rows, sorted_cols, desc, sort_nulls=SortNulls.never):
         columns to be first or last (or first when ASC, last when DESC, or
         vice versa), as specified.
     """
-    # TODO: temp debug!
-    print '  In sort...'
-    print '    sorted_cols:', sorted_cols
-    print '    desc       :', desc
-    print '    sort_nulls :', sort_nulls
-    print '    rows0 (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
     if not sorted_cols:
         rows.sort(cmp=safecmp)
         return
@@ -211,8 +177,6 @@ def sort(rows, sorted_cols, desc, sort_nulls=SortNulls.never):
 
     # Sort the final "group" (of rows with matching ORDER BY column values)
     rows[begin:] = sorted(rows[begin:], cmp=safecmp, key=unsorteds)
-    # TODO: temp debug!
-    print '    rows1 (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
 
     # Sort SQL NULL (Python None) values, in ORDER BY columns, in the
     # specified order (if any)
@@ -225,77 +189,31 @@ def sort(rows, sorted_cols, desc, sort_nulls=SortNulls.never):
                 if j < len(rows):
                     tmp = project_sorted(rows[j], sorted_cols[0:i+1])
                 if prev != tmp:
-                    # TODO: temp debug!
-                    print '      i, j, prev, tmp:', i, j, prev, tmp
                     if prev and prev[i] is None:
-                        # TODO: temp debug!
-                        print '        prev and prev[i] is None!'
                         if (   sort_nulls == SortNulls.first or
                               (sort_nulls == SortNulls.highest and desc[i]) or
                               (sort_nulls == SortNulls.lowest  and not desc[i])):
-                            # TODO: temp debug!
-                            print '        Sort nulls first!'
                             if i == 0:  # first ORDER BY column, so don't need to check others
-                                # TODO: temp debug!
-                                print '          Will move rows (begin, j, 0):', begin, j, 0
-                                print '    rows2a (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
                                 move_rows(rows, begin, j, 0)
-                                # TODO: temp debug!
-                                print '    rows3a (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
                                 break
                             ref = project_sorted(rows[begin], sorted_cols[0:i])
-                            # TODO: temp debug!
-                            print '      ref(first):', ref
                             for k in range(begin-1, -2, -1):
-                                # TODO: temp debug!
-                                maxk = max(k, 0)
-                                print '        i, j, k:', i, j, k
-                                print '        project_sorted(rows[k], sorted_cols[0:i]):\n        ', project_sorted(rows[maxk], sorted_cols[0:i])
                                 if (k < 0 or ref != project_sorted(rows[maxk], sorted_cols[0:i])):
-                                    # TODO: temp debug!
-                                    print '          Will move rows (begin, j, k+1):', begin, j, k+1
-                                    print '    rows2b (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
-                                    #rows = move_rows(rows, begin, j, k+1)
                                     move_rows(rows, begin, j, k+1)
-                                    # TODO: temp debug!
-                                    print '    rows3b (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
                                     break
                         elif ( sort_nulls == SortNulls.last or
                               (sort_nulls == SortNulls.lowest  and desc[i]) or
                               (sort_nulls == SortNulls.highest and not desc[i])):
-                            # TODO: temp debug!
-                            print '        Sort nulls last!'
                             if i == 0:  # first ORDER BY column, so don't need to check others
-                                # TODO: temp debug!
-                                print '          Will move rows (begin, j, len(rows)):', begin, j, len(rows)
-                                print '    rows2c (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
                                 move_rows(rows, begin, j, len(rows))
-                                # TODO: temp debug!
-                                print '    rows3c (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
                                 break
                             ref = project_sorted(rows[begin], sorted_cols[0:i])
-                            # TODO: temp debug!
-                            print '        rows[begin]:', rows[begin]
-                            print '        sorted_cols[0:i]:', sorted_cols[0:i]
-                            print '        ref(last):', ref
                             for k in range(j, len(rows)+1):
-                                # TODO: temp debug!
-                                mink = min(k, len(rows)-1)
-                                print '        k, rows[k]:', k, rows[mink]
-                                print '        project_sorted:', project_sorted(rows[mink], sorted_cols[0:i])
                                 if (k >= len(rows) or ref != project_sorted(rows[k], sorted_cols[0:i])):
-                                    # TODO: temp debug!
-                                    print '          Will move rows (begin, j, k):', begin, j, k
-                                    print '    rows2d (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
-                                    #rows = move_rows(rows, begin, j, k)
                                     move_rows(rows, begin, j, k)
-                                    # TODO: temp debug!
-                                    print '    rows3d (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
                                     break
                     prev = tmp
                     begin = j
-    # TODO: temp debug!
-    print '    rows4 (' + str(len(rows)) + '):\n     ', '\n      '.join(str(r) for r in rows)
 
 def parse_sql(x):
     """Finds if the SQL statement contains an ORDER BY command, and returns
@@ -331,10 +249,6 @@ def normalize(table, sql, sort_nulls=SortNulls.never):
 
     sql_upper = sql.upper()
     sort_cols = parse_sql(sql_upper)
-    # TODO: temp debug!
-    print '\nIn normalize...'
-    print '  table.columns.name:', map(lambda i: table.columns[i].name, range(len(table.columns)))
-    print '  sort_cols:', sort_cols
     indices = []
     desc = []
     if sort_cols:
@@ -345,18 +259,11 @@ def normalize(table, sql, sort_nulls=SortNulls.never):
                     indices.append(j)
                     desc.append(parse_for_order_by_desc(sql_upper, sort_cols[i]))
                     break
-    # TODO: temp debug!
-    print '  indices:', indices
-    print '  desc   :', desc
 
     # Make sure if there is an ORDER BY clause, the order by columns appear in
     # the result table. Otherwise all the columns will be sorted by the
     # normalizer.
-    # TODO: temp debug!
-    print 'table.tuples0 (' + str(len(table.tuples)) + '):\n     ', '\n      '.join(str(r) for r in table.tuples)
     sort(table.tuples, indices, desc, sort_nulls)
-    # TODO: temp debug!
-    print 'table.tuples1 (' + str(len(table.tuples)) + '):\n     ', '\n      '.join(str(r) for r in table.tuples)
 
     return table
 
